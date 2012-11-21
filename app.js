022fee0118e96fde5636c43524a1b2185131abf9
kuryaki <david.roncancio@gmail.com>
2012-11-21 16:28:48 -0500

    Register the job and queue it again after done

--- app.js
+++ app.js
@@ -2,6 +2,17 @@
 /**
  * Module dependencies.
  */
+var kue = require('kue')
+  , redis = require('redis')
+  , feedparser = require('feedparser');
+
+kue.redis.createClient = function() {
+  var client = redis.createClient(6379, '127.0.0.1');//change this to production data
+  return client;
+};
+
+var jobs = kue.createQueue();
+var minute = 10000;
 
 var express = require('express')
   , routes = require('./routes')
@@ -21,13 +32,34 @@ app.configure(function(){
   app.use(app.router);
   app.use(require('less-middleware')({ src: __dirname + '/public' }));
   app.use(express.static(path.join(__dirname, 'public')));
+  app.use(kue.app);
 });
+
 
 app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
 app.get('/', routes.index);
+
+app.post('/', routes.feed);
+
+jobs.promote();
+
+jobs.process('feed', function(job, done){
+  try {//this could fly if i validate the url via middleware/client
+    parser = feedparser.parseUrl(job.data.url, {addmeta:false}, function(error, feed){
+      if(error){
+        done(error);
+      }
+      console.log(feed.date);
+      jobs.create('feed', job.data).delay(minute).save();
+      done();
+    });
+  }catch(err){
+    done(err);
+  }
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,37 @@
-
+var kue = require('kue');
+var request = require('request');
+var minute = 10000;
 /*
  * GET home page.
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Subscribe to a feed',
+    subtitle: 'The alternative to pubsubhubsub'
+  });
+};
+
+exports.feed = function(req, response){
+  var data = {
+       type: 'feed',
+       data: {
+         title: ' subscription to ' + req.body.feed,
+         url: req.body.feed
+       },
+       options: {
+          _delay: minute
+       }
+     };
+
+  request
+    .defaults({body:data})
+    .post('http://localhost:3000/job', {json:true}, function(err, res, body){ //TODO change for production
+      response.render('index', {
+        title: 'Subscribe to a feed',
+        subtitle: 'The alternative to pubsubhubsub',
+        notice: req.body.callback + ' subscribed to ' + req.body.feed  + ' -> ' + res.body.message
+      });
+    });
+
 };
