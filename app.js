8baf7e2a3e0c5ca4a467c815e8315b0f3cfbb6f4
kuryaki <david.roncancio@gmail.com>
2012-12-11 13:04:58 -0500

    Testing push with filetypes and faster

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ if(config.db.auth){
 }
 
 var jobs = kue.createQueue();
-var minute = 10000;
+var minute = 60000;
 
 var express = require('express');
 var routes = require('./routes');
@@ -89,7 +89,7 @@ jobs.process('feed', function(job, done){
           if(!last_updated){ //Set the latest
             client.set(job.data.url, articles[0].pubDate, function(error, data){
               if(error){done(error);}
-              jobs.create('feed', job.data).delay(minute).save();
+              jobs.create('feed', job.data).delay(minute/6).save();
               done();
             });
           }else{
@@ -98,12 +98,12 @@ jobs.process('feed', function(job, done){
                 broadcast_feed(articles, last_updated, subscribers);
               }else{done(error);}
             });
-            jobs.create('feed', job.data).delay(minute).save();
+            jobs.create('feed', job.data).delay(minute/6).save();
             client.set(job.data.url, articles[0].pubDate);
             done();
           }
         }else{
-          jobs.create('feed', job.data).delay(minute).save();
+          jobs.create('feed', job.data).delay(minute/6).save();
           done('No Articles O.o!');
         }
 
